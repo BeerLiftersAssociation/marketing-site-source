d7422636a8d86a5e42a3b03c696e73a18f358b19
majway27 <robert.p.may@gmail.com>
2018-11-23 17:55:44 +0000

    #1 Build Process Migration

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #/bin/bash
+JEKYLL_ENV=production
+
 git pull
-jekyll build --environment production
+echo Enter commit message for source commit and push.  Enter when complete.
+read varname
+git add -A
+git commit -am "$varname"
+
+jekyll build --destination ../beerliftersassociation.github.io/
 cd ../beerliftersassociation.github.io/
 git add -A
 git commit -am "Deploy"
+git push
 cd ../marketing-site-source
 echo "Complete"
